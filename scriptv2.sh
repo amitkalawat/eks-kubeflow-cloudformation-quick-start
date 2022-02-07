71d5116f913863da459635e2b024986d0867774b
Kalawat <kwt@38f9d35bda57.ant.amazon.com>
2022-02-07 16:31:53 -0500

    Commit

--- scriptv2.sh
+++ scriptv2.sh
@@ -34,11 +34,12 @@ source kf-install.sh
 mkdir -p ${KF_DIR}
 cd ${KF_DIR} && wget -O kfctl_aws.yaml $CONFIG_URI
 
-sed -i "s@us-west-2@$AWS_REGION@" ${CONFIG_FILE}
-sed -i "s@#roles:@roles:@" ${CONFIG_FILE}
-#sed -i "s@- eksctl-${AWS_CLUSTER_NAME}-nodegroup-ng-a2-NodeInstanceRole-xxxxxxx@#- eksctl-${AWS_CLUSTER_NAME}-nodegroup-ng-a2-NodeInstanceRole-xxxxxxx@" ${CONFIG_FILE}
-sed -i "s@#- eksctl-kubeflow-aws-nodegroup-ng-a2-NodeInstanceRole-xxxxxxx@- $ROLE_NAME@" ${CONFIG_FILE}
-
+sed -i.bak -e "/region: us-west-2/ a \      enablePodIamPolicy: true" ${CONFIG_FILE}
+sed -i.bak -e "s@us-west-2@$AWS_REGION@" ${CONFIG_FILE}
+sed -i.bak -e "s@roles:@#roles:@" ${CONFIG_FILE}
+sed -i.bak -e "s@- eksctl-kubeflow-aws-nodegroup-ng-a2-NodeInstanceRole-xxxxxxx@#- eksctl-kubeflow-aws-nodegroup-ng-a2-NodeInstanceRole-xxxxxxx@" ${CONFIG_FILE}
+sed -i.bak -e "s@eksctl-kubeflow-aws-nodegroup-ng-a2-NodeInstanceRole-xxxxxxx@$ROLE_NAME@" ${CONFIG_FILE}
+sed -i.bak -e 's/kubeflow-aws/'"$AWS_CLUSTER_NAME"'/' ${CONFIG_FILE}
 
 curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/aws-iam-authenticator
 chmod +x aws-iam-authenticator
